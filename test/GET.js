ebbafbb7e6b2626cb0f06ab7a51fd72c19b29365
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-27 11:07:00 +1300

    add test for GET from server

---
+++ test/GET.js
@@ -0,0 +1,35 @@
+// this test the server GET request
+var test = require('tape')
+var request = require('supertest')
+var server = require('../server/server')
+
+//arrange the data
+var expectedPeanutButter = [ {
+    "id": 1,
+    "make": "Eta",
+    "description": "Crunchy"
+    },
+    {
+      "id": 2,
+      "make": "Eta",
+      "description": "Crunchy"
+    },
+    {
+      "id": 3,
+      "make": "Eta",
+      "description": "Crunchy"
+    }
+  ]
+
+  test("GET returns the peanutButter", function(t){
+    request(server).get('/peanutButter')
+    .expect('Content type', /JSON/)
+	  .expect(200)
+	  .end(function (err, res) {
+	  	t.equal(res.type, 'application/json', 'application/json')
+	  	t.equal(res.status, 200, 'status code 200')
+	  	t.equal(res.body.peanutButter.length, 3, 'correct length')
+	  	t.deepEqual(expectedPeanutButter, res.body.peanutButter, "the actual data and expected data are the same")
+	  })
+    t.end()
+  } )
