e3e3bca3844944c944dac270cab1cb069915d18a
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-27 15:29:56 +1300

    reorganise files, add client-side get request to create peanut butter list

---
+++ src/getRequest.js
@@ -0,0 +1,12 @@
+var $ = require('jquery')
+var request = require('superagent')
+
+module.exports = function(url,  callback){
+console.log('we are in getrequest', url, callback)
+	request
+	.get(url)     // the url is passed in so that the same get request can be used to return different data
+	.end(function(err, res) {
+		console.log(res.body)
+		callback(res.body)
+	})
+}
