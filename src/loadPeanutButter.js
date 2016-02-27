bd666df4c307b5013be9b0f2d5d8f2fcea6b654e
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-27 20:03:12 +1300

    handlebars templating on the client-side

--- src/loadPeanutButter.js
+++ src/loadPeanutButter.js
@@ -1,5 +1,6 @@
 var $ = require('jquery')
 var getRequest = require('./getRequest.js')
+var handlebars = require('handlebars')
 
 module.exports = function() {
   getRequest('http://localhost:3000/peanutButter', appendToList)
@@ -7,12 +8,26 @@ module.exports = function() {
   function appendToList(data){
 
     $('#table').text('')
-    $('#table').append('<table><tr><th>Make</th><th>Description</th></tr>')
-    data.forEach( (item) => {
-       $('#table').append('<tr><td>' + item.make + '</td><td>' + item.description + '</td></tr>')
-    })
-    $('#table').append('</table')
-    
+//    $('#table').append('<table><tr><th>Make</th><th>Description</th></tr>')
+    // data.forEach( (item) => {
+    //    $('#table').append('<tr><td>' + item.make + '</td><td>' + item.description + '</td></tr>')
+    // })
+    // $('#table').append('</table')
+
+  //   var shoesData = [{name:"Nike", price:199.00 }, {name:"Loafers", price:59.00 }, {name:"Wing Tip", price:259.00 }]
+  //   console.log(data, typeof data, shoesData, typeof shoesData)
+  // //Get the HTML from the template   in the script tag​
+  //  var theTemplateScript = $("#shoe-template").html()
+  //  var theTemplate = handlebars.compile(theTemplateScript)
+  //  $(".shoesNav").append(theTemplate(data))
+
+
+     var theTemplateScript = $("#pb-template").html()
+
+     //Compile the template​
+     var theTemplate = handlebars.compile(theTemplateScript);
+     $("#table").append(theTemplate(data));
+
   }
 
 }
